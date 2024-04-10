637461356b97e8a206067862764b8fb75b224ddd
tommaso-merlini <tommaso.merlini.2004@gmail.com>
2024-04-10 19:56:07 +0200

    feat: added custom font work sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./view/**/*.templ}", "./**/*.templ"],
-	safelist: [],
-	plugins: [require("daisyui")],
-	daisyui: {
-		themes: ["dark"]
-	}
+    content: ["./view/**/*.templ}", "./**/*.templ"],
+    safelist: [],
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ["dark"]
+    },
+    theme: {
+        extend: {
+            fontFamily: {
+                'work-sans': ['"Work Sans"', 'sans-serif'],
+            },
+        },
+    },
 }
